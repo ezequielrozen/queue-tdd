e79f7d416b291bce9144cbd67acd41b09ac48186
ezequiel <ezequielrozen@gmail.com>
2014-08-28 20:10:44 -0300

    cambio en un import

--- src/test/java/ColaTest.java
+++ src/test/java/ColaTest.java
@@ -1,8 +1,8 @@
 import org.junit.Test;
 
-import static junit.framework.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 public class ColaTest {
 
